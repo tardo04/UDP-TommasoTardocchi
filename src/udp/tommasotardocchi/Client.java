ff2aba768040c7643b3d06acdd4ab93eda285d82
tardo04 <73381541+tardo04@users.noreply.github.com>
2023-02-27 12:33:10 +0100

    Update Client.java

--- src/udp/tommasotardocchi/Client.java
+++ src/udp/tommasotardocchi/Client.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package udp.tommasotardocchi;
-
+import java.io.*;
+import java.net.*;
 /**
  *
  * @author Studenti
  */
 public class Client {
-    
+      public static void main(String args[]) throws Exepticion{
+        int portaServer = 6789; //porta del Server
+          InetAddres IPServer = InetAddres.getByName("localhost");
+          
+          byte[] bufferOUT = new byte[1024];
+          byte[] bufferIN = new byte[1024];
+          BufferReader input = new BufferedReader(new InputStreaReader(System.in));
+    }
+
+DatagramSocket clientSocket = new DatagramSocket();
+System.out.println("Client pronto - inserisci un dato da inviare:");
+String daSpedire = input.readLine();
+bufferOUT = daSpedire.getBytes();
+
+DatagramPacket sendPacket = new DatagramPacket(bufferOUT, bufferOUT.lenght, IPServer, portaServer);
+clientSocket.send(sendPacket);
+
+DtagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.lenght);
+clientSocket.receive(receivePacket);
+String ricevuto = new String(receivePacket.getData());
 }
